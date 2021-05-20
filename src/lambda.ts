65f2313199418ad42907c884a4c3dd58f77430f0
Pivithuru Jayakody <piveedj@gmail.com>
2021-05-21 02:23:40 +0530

    Create Serverless Lambda

---
+++ src/lambda.ts
@@ -0,0 +1,44 @@
+import * as express from 'express';
+
+import { Server } from 'http';
+
+import { Handler, Context } from 'aws-lambda';
+import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+import { AppModule } from './app.module';
+
+// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// due to a compressed response (e.g. gzip) which has not been handled correctly
+// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
+// binaryMimeTypes below
+const binaryMimeTypes: string[] = [];
+
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express();
+
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+
+    nestApp.use(eventContext());
+
+    await nestApp.init();
+
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+  return cachedServer;
+}
+
+export async function handler(event: any, context: Context): Handler {
+  cachedServer = await bootstrapServer();
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+}
