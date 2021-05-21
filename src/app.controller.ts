dbf6239dbc2515fdca9b2bb79c17b674e42ad673
Pivithuru Jayakody <piveedj@gmail.com>
2021-05-21 14:35:40 +0530

    Extend AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Logger, Req } from '@nestjs/common';
+import { AppService } from './app.service';
 import { LambdaRequest } from './aws';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   getEventAndContext(@Req() request: LambdaRequest): any {
     const ec = {
-      event: request?.apiGateway?.event,
-      context: request?.apiGateway?.context,
+      event: this.appService.getEvent(request),
+      context: this.appService.getContext(request),
     };
 
-    Logger.debug(ec, 'AppController');
+    Logger.debug(ec, 'AppController - Request - Event & Context');
 
     return ec;
   }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { LambdaRequest } from './aws';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  getEvent(request: LambdaRequest) {
+    return request?.apiGateway?.event;
+  }
+
+  getContext(request: LambdaRequest) {
+    return request?.apiGateway?.context;
+  }
+}
