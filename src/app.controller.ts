634e636fe2063064252dfa30d97ae9ff32cca541
Pivithuru Jayakody <piveedj@gmail.com>
2021-05-21 11:30:54 +0530

    Deploy to AWS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(':name')
+  getHelloWithName(@Param('name') name: string): string {
+    return this.appService.getHello(name);
+  }
+
+  @Get(':name/:age')
+  getHelloWithNameAndAge(
+    @Param('name') name: string,
+    @Param('age') age: string,
+    @Query() query: any,
+  ): string {
+    return this.appService.getHello(name, age, query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(name = 'World!', age = undefined, query = {}): string {
+    const { place } = query as any;
+    return `Hello ${name}!${age ? ` You' ${age} years old!` : ''}${
+      place ? ` You're from ${place}` : ''
+    }`;
   }
 }
